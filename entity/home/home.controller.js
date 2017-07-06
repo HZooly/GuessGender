a621cf65652b0a1bd2e9bc4458865506cbc20cbd
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2017-07-06 22:28:50 +0200

    Result notification + score in localStorage increment

--- entity/home/home.controller.js
+++ entity/home/home.controller.js
@@ -2,7 +2,7 @@ angular.module('gg').controller('homeController', homeController);
 
 homeController.$inject = ['$scope', '$route', '$http', 'peopleFactory'];
 
-function homeController($scope, $route, $http, peopleFactory){
+function homeController($scope, $route, $http, peopleFactory) {
 	var me = this;
 
 	////////////////
@@ -10,22 +10,44 @@ function homeController($scope, $route, $http, peopleFactory){
 	////////////////
 	me.getPeople = getPeople;
 	me.init = init;
+	me.checkGender = checkGender;
+	me.next = next;
 
 	////////////////
 	// VARIABLES //
 	////////////////
 	me.currentPeople;
+	me.score;
 
-	function getPeople(){
-		peopleFactory.init().$promise.then(function(response){
+	function getPeople() {
+		peopleFactory.init().$promise.then(function(response) {
 			me.currentPeople = response;
-		}).catch(function(error){
+		}).catch(function(error) {
 			alert('Error');
 		});
 	}
 
-	function init(){
+	function checkGender(gender) {
+		if (gender == me.currentPeople.gender) {
+			me.result = true;
+			localStorage.setItem('ggScore', parseInt(localStorage.getItem('ggScore'))+parseInt(1));
+			me.score = localStorage.getItem('ggScore');
+		} else {
+			me.result = false;
+		}
+	}
+
+	function next() {
+		me.result = null;
 		getPeople();
+	}
+
+	function init() {
+		me.result = null;
+		getPeople();
+		localStorage.removeItem('ggScore');
+		localStorage.setItem('ggScore', 0);
+		me.score = localStorage.getItem('ggScore');
 	}
 
 	init();
