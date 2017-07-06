35a034b1938cd1b8098a8d6d3522d6e29aab6924
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2017-07-06 18:50:59 +0200

    getPeople on view load - informations in Bulma card

--- entity/home/home.controller.js
+++ entity/home/home.controller.js
@@ -5,11 +5,28 @@ homeController.$inject = ['$scope', '$route', 'peopleFactory'];
 function homeController($scope, $route, peopleFactory){
 	var me = this;
 
+	////////////////
+	// FUNCTIONS //
+	////////////////
 	me.getPeople = getPeople;
+	me.init = init;
+
+	////////////////
+	// VARIABLES //
+	////////////////
+	me.currentPeople;
 
 	function getPeople(){
-		peopleFactory.init().$promise.then(function(a){
-			console.log(a);
+		peopleFactory.init().$promise.then(function(response){
+			me.currentPeople = response;
+		}).catch(function(error){
+			alert('Error');
 		});
 	}
+
+	function init(){
+		getPeople();
+	}
+
+	init();
 }
