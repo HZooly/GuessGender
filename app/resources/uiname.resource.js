ac287cc3cb1d772e5eae123e0ce61a7dbf1bad63
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2017-07-06 17:26:09 +0200

    resources pointing on uiname api

---
+++ app/resources/uiname.resource.js
@@ -0,0 +1,9 @@
+angular.module('gg').factory('uinameResource', uinameResource);
+
+uinameResource.$inject = ['$resource'];
+
+function uinameResource($resource){
+	return $resource('https://uinames.com/api/',{
+		'get': {method:'GET'}
+	});
+}
